27b6cc93c310f474687e0e31b8b01180b6a4ac08
Kopfstein <31157430+Kopfstein@users.noreply.github.com>
2025-08-15 11:09:05 +0000

    feat: integrate agent for response generation

--- src/double_agent/double_agent_app.py
+++ src/double_agent/double_agent_app.py
@@ -1,4 +1,22 @@
+import os
 import streamlit as st
+from smolagents import CodeAgent, OpenAIServerModel
+
+
+# Initialize a model
+model = OpenAIServerModel(
+    model_id = "deepseek/deepseek-r1-0528",
+    api_base = "https://openrouter.ai/api/v1",
+    api_key = os.getenv("OPENROUTER_API_KEY"),
+)
+
+# Create agent without tools
+agent = CodeAgent(
+    model=model,
+    name="Double Agent",
+    description="AI agent helping users to explore opposing views.",
+    tools=[],
+)
 
 # Follow tutorial: https://docs.streamlit.io/develop/tutorials/chat-and-llm-apps/build-conversational-apps
 
@@ -21,7 +39,8 @@ if prompt := st.chat_input("Enter your instructions"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo: {prompt}"
+    response = agent.run(prompt)
+
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         st.markdown(response)
